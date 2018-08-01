3d1f35dab4f90df92b8dfbdadea478b45f13ef64
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-08-01 11:50:37 +0200

    added init function

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // MODEL
-const budgetModel = (function() {
+const budgetModel = (function () {
 
 })();
 
 // VIEW
-const budgetView = (function() {
+const budgetView = (function () {
 
   // Strings for DOM selectors
   const DOMselectors = {
@@ -14,38 +14,50 @@ const budgetView = (function() {
     inputBtn: '.add__btn'
   }
 
-  // exposed public object
+  // Exposed public object
   return {
-    getInput: function() {
+    getInput: function () {
       return {
         type: document.querySelector(DOMselectors.inputType).value,
         description: document.querySelector(DOMselectors.inputDescription).value,
         value: document.querySelector(DOMselectors.inputValue).value
       }
     },
-    getDOMselectors: function() {
+    getDOMselectors: function () {
       return DOMselectors;
     }
-  }
+  };
 })();
 
 // APP CONTROLLER
-const budgetController = (function(budgetData, budgetUI) {
-  // Strings for DOM selectors
-  const DOMs = budgetUI.getDOMselectors();
-
+const budgetController = (function (budgetData, budgetUI) {
+  // Setup event listeners
+  const setupEventListeners = function () {
+    // Strings for DOM selectors
+    const DOMs = budgetUI.getDOMselectors();
+    // Event handlers
+    document.querySelector(DOMs.inputBtn).addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMs.inputDescription).addEventListener('keypress', function (event) {
+      if (event.key === 'Enter' || event.keyCode === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
   // Adding new item
-  function ctrlAddItem() { 
-    // get data from input fields
+  const ctrlAddItem = function () {
+    // Get data from input fields
     let input = budgetUI.getInput();
     console.log(input);
-  }
+  };
 
-  document.querySelector(DOMs.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.querySelector(DOMs.inputDescription).addEventListener('keypress', function(event) {
-    if (event.key === 'Enter' || event.keyCode === 13) {
-      ctrlAddItem();
+  return {
+    // Function to initialize whole application
+    init: function() {
+      console.log('Application has started');
+      setupEventListeners();
     }
-  });
+  };
+
 })(budgetModel, budgetView);
+
+budgetController.init();
