39f08de72a8b4410977ecb218b5e9b6bcbc696ae
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-08-01 10:52:54 +0200

    additions to view and controller, added more info in readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Budget-app
 
-Application to keep track of monthly income and expenses. Application is based on project designed by Jonas Schmedtmann for his course "The complete JavaScript Course".
+Application to keep track of monthly income and expenses. Module pattern is used to maintain separation of concerns. Application is based on project designed by Jonas Schmedtmann for his course "The complete JavaScript Course".
 
 Visit site: 
  <a href="https://danogo.github.io/budget-app/" target="_blank">budget-app</a> (use CTRL+click to open in new tab)
--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 // MODEL
-const budgetData = (function() {
+const budgetModel = (function() {
 
 })();
 
 // VIEW
-const budgetUI = (function() {
+const budgetView = (function() {
 
-});
+  // Strings for DOM selectors
+  const DOMselectors = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  // exposed public object
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMselectors.inputType).value,
+        description: document.querySelector(DOMselectors.inputDescription).value,
+        value: document.querySelector(DOMselectors.inputValue).value
+      }
+    },
+    getDOMselectors: function() {
+      return DOMselectors;
+    }
+  }
+})();
 
 // APP CONTROLLER
-const budgetController = (function(budgetModel, budgetView) {
+const budgetController = (function(budgetData, budgetUI) {
+  // Strings for DOM selectors
+  const DOMs = budgetUI.getDOMselectors();
 
   // Adding new item
   function ctrlAddItem() { 
+    // get data from input fields
+    let input = budgetUI.getInput();
+    console.log(input);
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOMs.inputBtn).addEventListener('click', ctrlAddItem);
 
-  document.querySelector('.add__description').addEventListener('keypress', function(event) {
+  document.querySelector(DOMs.inputDescription).addEventListener('keypress', function(event) {
     if (event.key === 'Enter' || event.keyCode === 13) {
       ctrlAddItem();
     }
   });
-})(budgetData, budgetUI);
+})(budgetModel, budgetView);
